57479b75e108f84e468137095569ae8953b4486f
Mansi Pal <mansipal2632007@gmail.com>
2026-05-17 21:54:59 +0530

    Time: 3 ms (56.93%), Space: 52.7 MB (36.04%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+        boolean[] visited = new boolean[arr.length];
+        
+        return dfs(arr, start, visited);
+    }
+    
+    private boolean dfs(int[] arr, int idx, boolean[] visited) {
+        
+       
+        if (idx < 0 || idx >= arr.length || visited[idx]) {
+            return false;
+        }
+        
+        // Found 0
+        if (arr[idx] == 0) {
+            return true;
+        }
+        
+        visited[idx] = true;
+        
+       
+        return dfs(arr, idx + arr[idx], visited) ||
+               dfs(arr, idx - arr[idx], visited);
+    }
+}
